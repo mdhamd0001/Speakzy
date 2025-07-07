ee42f26995377c134d2240b4563a413dc623af27
Mohd Hamd <mohdhamd2003@gmail.com>
2025-07-07 11:35:34 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://speakzy-backend.onrender.com/api" : "/api";
+const BASE_URL = import.meta.env.MODE === "development"
+  ? "http://localhost:5001/api"
+  : "https://speakzy-backend.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
