dc7c547245e962d3724db6444e2e39b7466c4aa1
Mohd Hamd <mohdhamd2003@gmail.com>
2025-07-07 12:19:49 +0530

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -82,12 +82,17 @@ export async function login(req, res) {
       expiresIn: "7d",
     });
 
-    res.cookie("jwt", token, {
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      httpOnly: true, // prevent XSS attacks,
-      sameSite: "strict", // prevent CSRF attacks
-      secure: process.env.NODE_ENV === "production",
-    });
+    // res.cookie("jwt", token, {
+    //   maxAge: 7 * 24 * 60 * 60 * 1000,
+    //   httpOnly: true, // prevent XSS attacks,
+    //   sameSite: "strict", // prevent CSRF attacks
+    //   secure: process.env.NODE_ENV === "production",
+    // });
+    res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,      // Required for HTTPS
+  sameSite: "none",  // Required for cross-site cookies
+  });
 
     res.status(200).json({ success: true, user });
   } catch (error) {
